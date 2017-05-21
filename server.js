92d4ff182f6717a5a553bece4a53b204af9a1803
Eric Papagiannis <eric.papagiannis@mail.utoronto.ca>
2017-05-21 14:56:35 -0400

    Switched from MongoDB to mongoose, and created a function to get messages

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
-var mongo = require('mongodb').MongoClient;
+var mongoose = require('mongoose')
 
-var database;
+var Message = mongoose.model('Message', {
+  msg: String
+});
 
 app.use(bodyParser.json());
 app.use(function(req, res, next) {
@@ -15,16 +17,23 @@ app.use(function(req, res, next) {
 
 app.post('/api/message', function(req, res){
   console.log(req.body);
-  //database.collection('messages').insertOne(req.body)
+  var message = new Message(req.body);
+  message.save();
   res.status(200);
 })
 
-mongo.connect("mongodb://localhost:27017/test", function(err, db) {
+mongoose.connect("mongodb://localhost:27017/test", function(err, db) {
   if (!err) {
     console.log("we are connected to mongo")
-    database = db;
+    GetMessages();
   }
 })
+
+function GetMessages() {
+  Message.find({}).exec(function(err, result) {
+    console.log(result);
+  }
+)}
 
 var server = app.listen(5000, function() {
   console.log('listening on port ', server.address().port)
