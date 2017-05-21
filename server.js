c7e638ff5c103ce2c1c4693bfa0cd1f7b2b30eb3
Eric Papagiannis <eric.papagiannis@mail.utoronto.ca>
2017-05-21 14:45:56 -0400

    Configured mongo database to store the posted messages

--- server.js
+++ server.js
@@ -1,18 +1,29 @@
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
+var mongo = require('mongodb').MongoClient;
+
+var database;
 
 app.use(bodyParser.json());
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+  res.header("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
   next();
 });
 
 app.post('/api/message', function(req, res){
   console.log(req.body);
+  //database.collection('messages').insertOne(req.body)
   res.status(200);
+})
+
+mongo.connect("mongodb://localhost:27017/test", function(err, db) {
+  if (!err) {
+    console.log("we are connected to mongo")
+    database = db;
+  }
 })
 
 var server = app.listen(5000, function() {
